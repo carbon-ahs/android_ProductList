0df704e22eeb173739dc48e34664c9231e037a15
carbon-ahs <shehanuk.ahsan@gmail.com>
2024-04-22 17:52:35 +0600

    listAdapter done

--- app/src/main/java/com/axiagroups/productlist/MainActivity.java
+++ app/src/main/java/com/axiagroups/productlist/MainActivity.java
@@ -7,8 +7,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.axiagroups.productlist.adapter.ProductListAdapter;
 
 public class MainActivity extends AppCompatActivity {
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,9 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        final ProductListAdapter adapter = new ProductListAdapter(new ProductListAdapter.ProductDiff());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
--- app/src/main/java/com/axiagroups/productlist/adapter/ProductListAdapter.java
+++ app/src/main/java/com/axiagroups/productlist/adapter/ProductListAdapter.java
@@ -1,17 +1,70 @@
 package com.axiagroups.productlist.adapter;
 
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class ProductListAdapter  {
+import com.axiagroups.productlist.R;
+import com.axiagroups.productlist.model.Product;
+
+// https://developer.android.com/codelabs/android-room-with-a-view#11
+public class ProductListAdapter extends ListAdapter<Product, ProductViewHolder> {
+    public ProductListAdapter(@NonNull DiffUtil.ItemCallback<Product> diffCallback) {
+        super(diffCallback);
+    }
+
+    @NonNull
+    @Override
+    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return ProductViewHolder.create(parent);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProductViewHolder holder, int position) {
+        Product current = getItem(position);
+        holder.bind(current.getName(),String.valueOf(current.getPrice()));
+    }
+
+    public static class ProductDiff extends DiffUtil.ItemCallback<Product> {
+
+        @Override
+        public boolean areItemsTheSame(@NonNull Product oldItem, @NonNull Product newItem) {
+            return oldItem == newItem;
+        }
+
+        @Override
+        public boolean areContentsTheSame(@NonNull Product oldItem, @NonNull Product newItem) {
+            return oldItem.getName().equals(newItem.getName());
+        }
+    }
 }
 
 class ProductViewHolder extends RecyclerView.ViewHolder{
+    private final TextView productNameTv, productPriceTv;
+
 
     public ProductViewHolder(@NonNull View itemView) {
         super(itemView);
+        productNameTv = itemView.findViewById(R.id.row_productNameTV);
+        productPriceTv = itemView.findViewById(R.id.row_productPriceTV);
+    }
+
+    public void bind(String name, String price) {
+        productNameTv.setText(name);
+        productPriceTv.setText(price);
+    }
+
+    static ProductViewHolder create(ViewGroup parent){
+
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recyclerview_item, parent, false);
+        return new ProductViewHolder(view);
     }
 }
