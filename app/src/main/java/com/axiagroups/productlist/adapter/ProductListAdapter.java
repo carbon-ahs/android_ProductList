43ca6317156eaa2f12970c4a986e79a761ec0fe5
carbon-ahs <shehanuk.ahsan@gmail.com>
2024-04-22 17:12:52 +0600

    listAdapter Ongoing

---
+++ app/src/main/java/com/axiagroups/productlist/adapter/ProductListAdapter.java
@@ -0,0 +1,17 @@
+package com.axiagroups.productlist.adapter;
+
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ProductListAdapter  {
+}
+
+class ProductViewHolder extends RecyclerView.ViewHolder{
+
+    public ProductViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
