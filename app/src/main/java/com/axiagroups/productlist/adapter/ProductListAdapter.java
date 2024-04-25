2efd949e76b0f33df42c19fc6f55b403f52c198a
carbon-ahs <shehanuk.ahsan@gmail.com>
2024-04-25 19:21:52 +0600

    edit & delete icon onclick listener added

--- app/src/main/java/com/axiagroups/productlist/adapter/ProductListAdapter.java
+++ app/src/main/java/com/axiagroups/productlist/adapter/ProductListAdapter.java
@@ -4,7 +4,9 @@ package com.axiagroups.productlist.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.DiffUtil;
@@ -29,7 +31,8 @@ public class ProductListAdapter extends ListAdapter<Product, ProductViewHolder> 
     @Override
     public void onBindViewHolder(@NonNull ProductViewHolder holder, int position) {
         Product current = getItem(position);
-        holder.bind(current.getName(),String.valueOf(current.getPrice()));
+        holder.bind(current.getName(), String.valueOf(current.getPrice()));
+
     }
 
     public static class ProductDiff extends DiffUtil.ItemCallback<Product> {
@@ -46,22 +49,42 @@ public class ProductListAdapter extends ListAdapter<Product, ProductViewHolder> 
     }
 }
 
-class ProductViewHolder extends RecyclerView.ViewHolder{
+class ProductViewHolder extends RecyclerView.ViewHolder {
     private final TextView productNameTv, productPriceTv;
+    ImageView editIcon, deleteIcon;
 
 
     public ProductViewHolder(@NonNull View itemView) {
         super(itemView);
         productNameTv = itemView.findViewById(R.id.row_productNameTV);
         productPriceTv = itemView.findViewById(R.id.row_productPriceTV);
+        editIcon = itemView.findViewById(R.id.editIcon);
+        deleteIcon = itemView.findViewById(R.id.deleteIcon);
     }
 
     public void bind(String name, String price) {
         productNameTv.setText(name);
         productPriceTv.setText(price);
+
+        editIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), getAdapterPosition() + " Edit Item is clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        deleteIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "Delt Item is clicked", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
     }
 
-    static ProductViewHolder create(ViewGroup parent){
+    static ProductViewHolder create(ViewGroup parent) {
 
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.recyclerview_item, parent, false);
